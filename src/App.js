bae14d3683d46167f829758a2c2369f0d3d6909e
jfranklin12 <j.franks725@gmail.com>
2023-01-05 18:53:52 -0800

    homepage style changes and responsive

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <Homepage />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -2,7 +2,7 @@ import './homepage.css';
 
 function Homepage() {
   return (
-    <div className="grid-container">
+    <div className="grid-container grid-container--home">
       <div>
       <h1 className='text-accent fs-500 ff-sans-cond uppercase letter-spacing-1'>Cross Country Road Trip
       <span className='d-block fs-900 ff-serif text-white'>Atlanta </span>
@@ -10,7 +10,7 @@ function Homepage() {
       <span className='d-block fs-900 ff-serif text-white'>Santa Barbara</span></h1>
       </div>
       <div>
-      <a href="#" className="large-button uppercase ff-serif fs-600 text-dark bg-white">Travel</a>
+      <a href="#" className="large-button uppercase ff-serif text-dark bg-white">Travel</a>
       </div>
     </div>
   );
