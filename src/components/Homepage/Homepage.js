499db2ff0de4ce30b91cabdf13a6ec89bb839c8b
jfranklin12 <j.franks725@gmail.com>
2023-01-05 18:17:45 -0800

    layout changes to homepage

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -4,7 +4,10 @@ function Homepage() {
   return (
     <div className="grid-container">
       <div>
-      <h1 className='text-accent fs-500 ff-sans-cond uppercase letter-spacing-1'>Cross Country Road Trip <span className='fs-900 ff-serif text-white'>Atlanta </span>to <span className='fs-900 ff-serif text-white'>Santa Barabara</span></h1>
+      <h1 className='text-accent fs-500 ff-sans-cond uppercase letter-spacing-1'>Cross Country Road Trip
+      <span className='d-block fs-900 ff-serif text-white'>Atlanta </span>
+      to
+      <span className='d-block fs-900 ff-serif text-white'>Santa Barbara</span></h1>
       </div>
       <div>
       <a href="#" className="large-button uppercase ff-serif fs-600 text-dark bg-white">Travel</a>
