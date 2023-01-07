179ba251c9868a19d2dc86fc904b71b295ffa044
jfranklin12 <j.franks725@gmail.com>
2023-01-06 18:27:24 -0800

    background image added for mobile and desktop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Homepage from './components/Homepage/Homepage';
 import Footer from './components/Footer';
+import backgroundDesktop from './images/background-grand-canyon.JPEG';
+import backgroundMobile from './images/background-mobile.JPEG';
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const backgroundImage = windowWidth >= 768 ? backgroundDesktop : backgroundMobile;
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        }
+    }, []);  
+
   return (
-    <div>
+    <div style={{backgroundImage: `url(${backgroundImage})`, backgroundPosition: `bottom center`, backgroundSize: `cover`}}>
       <Navbar />
       <Homepage />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
