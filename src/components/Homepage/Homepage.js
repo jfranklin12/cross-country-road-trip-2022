86367dc539f7a8c44548920288d07783ddb29d82
jfranklin12 <j.franks725@gmail.com>
2023-01-27 15:28:42 -0800

    map integrated to travel page

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -11,7 +11,7 @@ function Homepage() {
       {/* <p>Follow our trip across the country! We learned a lot about traveling from this trip, and we want to share it with you! Check out all the places we stopped, and other cool places we visited!</p> */}
       </div>
       <div className='align-right'>
-      <a href="#" className="large-button uppercase ff-serif text-dark bg-white">Travel</a>
+      <a href="/travel" className="large-button uppercase ff-serif text-dark bg-white">Travel</a>
       </div>
     </div>
   );
--- src/components/pages/Travel.js
+++ src/components/pages/Travel.js
@@ -1,7 +1,31 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import './travel.css';
+
 function Travel() {
+
+    const position = [33.753746, -84.386330]
     return (
-        <div>
-            <h1>Travel</h1>
+        <div className='grid-container'>
+            <div>
+                <h1 className='text-accent fs-500 ff-sans-cond uppercase letter-spacing-1'>We started in
+                <span className='d-block fs-900 ff-serif text-white'>Atlanta, GA </span>
+                and traveled to
+                {/* <span className='d-block fs-900 ff-serif text-white'>Tunica, MS </span>
+                <span className='d-block fs-900 ff-serif text-white'>Durrant, OK </span>
+                <span className='d-block fs-900 ff-serif text-white'>Amarillo, TX </span>
+                <span className='d-block fs-900 ff-serif text-white'>Amarillo, TX </span> */}
+                </h1>
+            </div>
+            <MapContainer id="map" center={position} zoom={5} scrollWheelZoom={false}>
+                <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+
         </div>
     );
 };
