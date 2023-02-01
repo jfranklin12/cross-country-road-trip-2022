7f0d956913f58649159a081f7f7119c3d4c162c5
jfranklin12 <j.franks725@gmail.com>
2023-02-01 13:38:58 -0800

    initial visit page layout

--- src/components/pages/Visit.js
+++ src/components/pages/Visit.js
@@ -1,6 +1,28 @@
+import atlanta from "../../images/atlanta.JPEG"; 
 function Visit() {
     return (
-<h1>Visit</h1>
+        <div className="grid-container">
+            {/* main image for location */}
+            <div>
+                <img atl="Atlanta, GA" src={atlanta}></img>
+            </div>
+            {/* state tabs */}
+            <div>
+                {/* tabs */}
+                <div>
+                    <ul>
+                        <li>
+                        <a className="ff-sans-cond uppercase text-white letter-spacing-2" href="/">Georgia</a>
+                        </li>
+                    </ul>
+                    <h1>Georgia</h1>
+                    <p>info</p>
+                </div>
+            </div>
+
+
+
+        </div>
 
     )
 };
