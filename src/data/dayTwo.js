531a9c80cd6c97355fdc4f0972c7ab3604ae7f23
jfranklin12 <j.franks725@gmail.com>
2023-02-14 09:43:32 -0800

    info added about Arkansas

--- src/data/dayTwo.js
+++ src/data/dayTwo.js
@@ -7,7 +7,7 @@ const dayTwo = [
         date: 'November 30, 2022',
         location: 'Arkansas',
         stops: 'Hot Springs',
-        description: `Arkansas information`,
+        description: `Arkansas is a beautiful state! From Tunica, it's a 4 hour drive to Hot Springs, Arkanasas. After driving through Memphis, We took I-40 to I-30, passing through Little Rock. From I-30, we exited to route 70. This was one of the most spectacular drives I have ever done. The time of year created a blanket of orange, yellow, red, and green leaves that covered ever inch of the hill sides down into Hot Springs.`,
         image: atlanta,
     },
         
