3a9e36899e1e32826c3fe14ec0d92ac966918567
jfranklin12 <j.franks725@gmail.com>
2023-02-15 14:31:39 -0800

    more information added for hot springs

--- src/data/dayTwo.js
+++ src/data/dayTwo.js
@@ -7,7 +7,7 @@ const dayTwo = [
         date: 'November 30, 2022',
         location: 'Arkansas',
         stops: 'Hot Springs',
-        description: `Arkansas is a beautiful state! From Tunica, it's a 4 hour drive to Hot Springs, Arkanasas. After driving through Memphis, We took I-40 to I-30, passing through Little Rock. From I-30, we exited to route 70. This was one of the most spectacular drives I have ever done. The time of year created a blanket of orange, yellow, red, and green leaves that covered ever inch of the hill sides down into Hot Springs.`,
+        description: `Arkansas is a beautiful state! From Tunica, it's a 4 hour drive to Hot Springs, Arkanasas. After driving through Memphis, We took I-40 to I-30, passing through Little Rock. From I-30, we exited to route 70. This was one of the most spectacular drives I have ever done. The time of year created a blanket of orange, yellow, red, and green leaves that covered ever inch of the hill sides down into Hot Springs. Once we arrived in Hot Springs, we dropped off the the trailer in the ample (free!) parking they had outside of town, and headed off in my Civic to explore this national park! We stopped by the visitor center first. The employees were super kind and helpful. We walked down a brick path just behind the main road of Hot Springs, and saw some of the beautiful natural hot springs that are all over this park. We jumped back in the car and drove up the small mountain. There's a 1.5 mile trail that loops around the top, and we caught some beautiful sights! There was so much to do in this town, but it was time for us to get back on the road. I'm sure we'll be back!`,
         image: atlanta,
     },
         
