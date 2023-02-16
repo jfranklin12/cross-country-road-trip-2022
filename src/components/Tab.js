29d9adf5efba889a71933f75ed15ceb1a98c3834
jfranklin12 <j.franks725@gmail.com>
2023-02-15 16:10:24 -0800

    style added to day 2 and more information added to OK

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -9,83 +9,83 @@ function TabComponent() {
     return (
         <Tabs forceRenderTabPanel defaultIndex={1}>
 
-        {/* List of Days */}
-        <TabList className='text-white fs-600 ff-sans-cond uppercase letter-spacing-4 t-list'>
-        {days.map(({ id, day }) => {
+            {/* List of Days */}
+            <TabList className='text-white fs-600 ff-sans-cond uppercase letter-spacing-4 t-list'>
+                {days.map(({ id, day }) => {
                     return (
                         <Tab key={id} className='tabs'>
                             {day}
                         </Tab>
                     )
                 })}
-        </TabList>
+            </TabList>
 
-        {/* Day One Location tabs and information */}
-        <TabPanel>
-          <Tabs forceRenderTabPanel>
-            <TabList className='text-white fs-400 ff-sans-cond uppercase letter-spacing-5 t-list'>
-            {dayOne.map(({ id, location}) => {
+            {/* Day One Location tabs and information */}
+            <TabPanel>
+                <Tabs forceRenderTabPanel>
+                    <TabList className='text-white fs-400 ff-sans-cond uppercase letter-spacing-5 t-list'>
+                        {dayOne.map(({ id, location }) => {
                             return (
-                                <Tab key= {id} className='tabs'>{location}</Tab>
+                                <Tab key={id} className='tabs'>{location}</Tab>
                             )
                         })}
-            </TabList>
-                      {dayOne.map(({ id, stops, description, location, image}) => {
+                    </TabList>
+                    {dayOne.map(({ id, stops, description, location, image }) => {
                         return (
                             <TabPanel key={id}>
                                 <h1 className="fs-700 ff-serif uppercase letter-spacing-1 panel-title">{stops}</h1>
-                                <img alt={location} src={image} className="tab-img"/>
+                                <img alt={location} src={image} className="tab-img" />
                                 <p className="letter-spacing-4 para">{description}</p>
                             </TabPanel>
                         )
                     })}
-          </Tabs>
-        </TabPanel>
+                </Tabs>
+            </TabPanel>
 
-        {/* Day Two Location Tabs and information */}
-        <TabPanel>
-          <Tabs forceRenderTabPanel>
-            <TabList>
-            {dayTwo.map(({ id, location}) => {
+            {/* Day Two Location Tabs and information */}
+            <TabPanel>
+                <Tabs forceRenderTabPanel>
+                    <TabList>
+                        {dayTwo.map(({ id, location }) => {
                             return (
-                                <Tab key= {id}>{location}</Tab>
+                                <Tab key={id} className='tabs'>{location}</Tab>
                             )
                         })}
-            </TabList>
-            {dayTwo.map(({ id, stops, description, location, image}) => {
+                    </TabList>
+                    {dayTwo.map(({ id, stops, description, location, image }) => {
+                        return (
+                            <TabPanel key={id}>
+                                <h1 className="fs-700 ff-serif uppercase letter-spacing-1 panel-title">{stops}</h1>
+                                <img alt={location} src={image} className="tab-img" />
+                                <p className="letter-spacing-4 para">{description}</p>
+                            </TabPanel>
+                        )
+                    })}
+                </Tabs>
+            </TabPanel>
+
+            {/* Day Three Location Tabs and information */}
+            <TabPanel>
+                <Tabs forceRenderTabPanel>
+                    <TabList>
+                        {dayThree.map(({ id, location }) => {
+                            return (
+                                <Tab key={id}>{location}</Tab>
+                            )
+                        })}
+                    </TabList>
+                    {dayThree.map(({ id, stops, description, location, image }) => {
                         return (
                             <TabPanel key={id}>
                                 <h1>{stops}</h1>
-                                <img alt={location} src={image}/>
+                                <img alt={location} src={image} />
                                 <p>{description}</p>
                             </TabPanel>
                         )
                     })}
-          </Tabs>
-        </TabPanel>
-
-        {/* Day Three Location Tabs and information */}
-        <TabPanel>
-          <Tabs forceRenderTabPanel>
-            <TabList>
-            {dayThree.map(({ id, location}) => {
-                            return (
-                                <Tab key= {id}>{location}</Tab>
-                            )
-                        })}
-            </TabList>
-            {dayThree.map(({ id, stops, description, location, image}) => {
-                        return (
-                            <TabPanel key={id}>
-                                <h1>{stops}</h1>
-                                <img alt={location} src={image}/>
-                                <p>{description}</p>
-                            </TabPanel>
-                        )
-                    })}
-          </Tabs>
-        </TabPanel>
-      </Tabs>
+                </Tabs>
+            </TabPanel>
+        </Tabs>
     );
 
 }
--- src/data/dayTwo.js
+++ src/data/dayTwo.js
@@ -10,6 +10,15 @@ const dayTwo = [
         description: `Arkansas is a beautiful state! From Tunica, it's a 4 hour drive to Hot Springs, Arkanasas. After driving through Memphis, We took I-40 to I-30, passing through Little Rock. From I-30, we exited to route 70. This was one of the most spectacular drives I have ever done. The time of year created a blanket of orange, yellow, red, and green leaves that covered ever inch of the hill sides down into Hot Springs. Once we arrived in Hot Springs, we dropped off the the trailer in the ample (free!) parking they had outside of town, and headed off in my Civic to explore this national park! We stopped by the visitor center first. The employees were super kind and helpful. We walked down a brick path just behind the main road of Hot Springs, and saw some of the beautiful natural hot springs that are all over this park. We jumped back in the car and drove up the small mountain. There's a 1.5 mile trail that loops around the top, and we caught some beautiful sights! There was so much to do in this town, but it was time for us to get back on the road. I'm sure we'll be back!`,
         image: atlanta,
     },
+    {
+        id: 2,
+        day: 'Day 2',
+        date: 'November 30, 2022',
+        location: 'Oklahoma',
+        stops: 'Atoka',
+        description: `The next place we wanted to visit was Chickasaw National Recreation Area. We were super excited to see the buffalo! We drove from Hot Springs to a small town in Oklanhoma called Atoka taking Route 70 to Highway 3. We left Hot Springs just as the sun was setting, which meant that we had 4 hours of driving ahead of us in the dark. Route 70 and Highway 3 are small highways with limited lighting. We learned a lot on this leg of the trip. Driving without light and the trailer was super hard, and driving without a planned break is a no-go. Around 8 pm, we arrived at our hotel. Atoka was safe and friendly and small. We stayed at the Comfort Inn, which was a great place to rest for the night. We grabbed dinner and I began to plan the next day. I made sure to find lots of places to stop to explore every couple hours, and limit our nighttime driving. Then we hit the hay and got ready for another big day!`,
+        image: atlanta,
+    },
         
 ];
 
