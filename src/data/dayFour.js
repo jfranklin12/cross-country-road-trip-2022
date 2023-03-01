06722732623be289a4d7e48b3f01ec14d0ee578b
jfranklin12 <j.franks725@gmail.com>
2023-03-01 10:48:48 -0800

    day four file added with skelton outline

---
+++ src/data/dayFour.js
@@ -0,0 +1,40 @@
+import atlanta from "../images/dayOne/atlanta.JPEG";
+
+const dayFour = [
+    {
+        id: 1,
+        day: 'Day 4',
+        date: 'December 2, 2022',
+        location: 'New Mexico',
+
+        stopOne: 'New Mexico Border',
+        descriptionOne: `Pit Stop`,
+        imageOne: atlanta,
+
+        stopTwo: 'Blue Lake',
+        descriptionTwo: `Blue Lake`,
+        imageTwo: atlanta,
+
+        stopThree: 'Albuquerque',
+        descriptionThree: `Albuquerque Info`,
+        imageThree: atlanta,
+    },
+    {
+        id: 1,
+        day: 'Day 4',
+        date: 'December 2, 2022',
+        location: 'Arizona',
+
+        stopOne: 'Arizona Border',
+        descriptionOne: `Pit Stop`,
+        imageOne: atlanta,
+
+        stopTwo: 'Holbrook',
+        descriptionTwo: `Hotel`,
+        imageTwo: atlanta,
+    },
+      
+        
+];
+
+export default dayFour;
